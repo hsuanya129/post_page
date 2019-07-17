3a9c8a7779b877b3d93b8633b3535a3e5592ee91
hsuanya129 <37055177+hsuanya129@users.noreply.github.com>
2019-07-17 16:22:15 +0800

    stuck at submitPost

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Route, Link, history,Redirect} from 'react-router-dom'
 
 //for index page
 class IndexPage extends React.Component{
   constructor(props){
     super(props);
+    this.toPostPage = this.toPostPage.bind(this);
+    this.toRemovePost = this.toRemovePost.bind(this);
+  }
+
+  toPostPage(){
+    this.props.history.push('/add_post')
+  }
+
+  toRemovePost(){
+
   }
 
   render(){
     return(
       <div className="App">
-          <button><Link to="/add_post/">Add Post</Link></button>
+          <button onClick={this.toPostPage}>Add Post</button>
           <button>Remove Post</button>
-          <Posts></Posts>
+          <Posts />
       </div>
     )
   }
@@ -30,7 +40,8 @@ class App extends React.Component{
       <div className="App">
         <BrowserRouter>
           <Route exact path="/" component={IndexPage}/>
-          <Route path="/add_post/" component={NewPost}/>
+          <Route path="/add_post" component={NewPost}/>
+          
         </BrowserRouter>
       </div>
     )
@@ -41,10 +52,7 @@ class App extends React.Component{
 class NewPost extends React.Component{
   constructor(props){
     super(props);
-    this.state={
-      titleValue:'',
-      contentValue:'Please input Content.'
-    }
+    
     this.submitPost = this.submitPost.bind(this);
     this.uploadPhoto = this.uploadPhoto.bind(this);
 
@@ -52,8 +60,12 @@ class NewPost extends React.Component{
 
   submitPost(event){
     //save post and redirect
+    const title = document.getElementById('titleBox');
+    const content = document.getElementById('contentBox');
+    this.props.history.push('/')
     
-    event.preventDefault();
+
+
   }
 
   uploadPhoto(){
@@ -63,15 +75,14 @@ class NewPost extends React.Component{
   render(){
     return(
       <div>
-        <form onSubmit={this.submitPost}>
           <p>Title:</p>
           <input type="text" id="titleBox" />
           <p>Content:</p>
-          <input type="text" id="contentBox" />
+          <textarea id="contentBox" defaultValue="Please input Content."/>
           <br/>
-          <input id='files' type="file" name="pic" accept="image/*" />
-          <input type='submit' value='submit'/>
-        </form>
+          <input id='file-upload' type="file" name="pic" accept="image/*"  />
+          <button id="submitPost" onClick={this.submitPost} >submit</button>
+          <Route path="/post" component={Posts} />
       </div>
     )
   }
@@ -81,28 +92,52 @@ class NewPost extends React.Component{
 class Post extends React.Component{
   constructor(props){
     super(props);
+  
   }
 
   render(){
+    console.log(`${this.props.title} ${this.props.content}`)
     return(
       <div>
-        <p>I'm a single post</p>
+        <p>{this.props.title}</p>
+        <p>{this.props.content}</p>
+        
       </div>
+      
     )
   }
 }
  
 
-// for posts div
+//for posts div, single source of truth, 
+//state store all posts' contents, while props store every single new post's content
 class Posts extends React.Component{
   constructor(props){
     super(props);
+    this.state={
+      itemList:[
+        {
+          title:'myTitle',
+        content:'myContent'
+        }
+      ]
+    }
+  }
+
+  addItem(){
+    const newList = this.state.itemList.concat({title:this.props.title,
+      content:this.props.content})
+    this.setState={
+      itemList:newList
+    }
   }
 
   render(){
+    const itemList = this.state.itemList.map((item,index)=><Post key={index} title={item.title} content={item.content} />)
     return(
       <div>
-        <Post></Post>
+        {itemList}
+        
       </div>
     )
   }
